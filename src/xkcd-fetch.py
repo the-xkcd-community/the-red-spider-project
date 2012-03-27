73b6a1ea336e1491a4e2977b19d3e71036e6b43a
Neil Forrester <nforrest@mit.edu>
2012-03-27 18:34:43 -0400

    Bug fix
    I made a silly.

--- src/xkcd-fetch.py
+++ src/xkcd-fetch.py
@@ -3,16 +3,6 @@
 # Copyright 2012 Neil Forrester
 # See License.txt
 
-import os
-import sys
-
-def set_red_spider_root():
-	os.putenv('RED_SPIDER_ROOT', os.path.split(os.path.abspath(os.path.dirname(sys.argv[0])))[0])
-
-if __name__ == "__main__":
-	set_red_spider_root()
-	os.system(" ".join(sys.argv[1:]))
-
 from urllib import urlretrieve
 import re
 import argparse
