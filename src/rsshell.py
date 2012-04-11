ff584c3221ade7e8ee416694fd745453192f4641
Neil Forrester <nforrest@mit.edu>
2012-04-11 17:23:15 -0400

    Fixed bug pointed out by Julian

--- src/rsshell.py
+++ src/rsshell.py
@@ -10,17 +10,18 @@ Ideas for future changes (unordered):
  -  add variables like PATH and PYTHONPATH;
  -  use the PATHEXT variable on windows (strip off the file extension on POSIX);
  -  cd to RED_SPIDER_ROOT/work, mkdir if it doesn't exist;
- -  drop rscall, since rsshell can already mimic its behaviour;
  -  show a short info message on launch.
 '''
 
 import os
 import sys
-import rscall
+
+def set_red_spider_root():
+	os.putenv('RED_SPIDER_ROOT', os.path.split(os.path.abspath(os.path.dirname(sys.argv[0])))[0])
 
 def main (argv = None):
-    rscall.set_red_spider_root()
-    if argv and len(argv) > 1:                  # behave like rscall
+    set_red_spider_root()
+    if argv and len(argv) > 1:                  # call the requested program
         return os.system(" ".join(argv[1:]))
     if os.name == 'nt':                         # Windows
         return os.system(os.getenv('COMSPEC', 'cmd.exe'))
