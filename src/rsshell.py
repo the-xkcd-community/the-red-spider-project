997c3737ce37c53407cb7ba5eab8149b3f705839
Julian Gonggrijp <j.gonggrijp@gmail.com>
2012-04-11 15:31:20 +0200

    Make rsshell more portable
    Primary changes (with direct regard to portability):
     -  use os.name to determine what to do (COMSPEC on Windows versus
        SHELL on unixy systems);
     -  use /usr/bin/env python instead of hard-coded /usr/bin/python on
        the shebang for more flexibility on unixy systems.
    
    Secondary changes, related to the dispatch on os.name:
     -  move the program body into the main function;
     -  use command line arguments.
    
    Unrelated changes:
     -  add a comment to announce ideas for future changes;
     -  add yours truly to the copyright declaration.

--- src/rsshell.py
+++ src/rsshell.py
@@ -1,13 +1,31 @@
-#!/usr/bin/python
+#! /usr/bin/env python
 
-# Copyright 2012 Neil Forrester
+# Copyright 2012 Neil Forrester, Julian Gonggrijp
 # Licensed under the Red Spider Project License.
 # See the License.txt that shipped with your copy of this software for details.
+
+'''
+Ideas for future changes (unordered):
+ -  use the subprocess module instead of os.system;
+ -  add variables like PATH and PYTHONPATH;
+ -  use the PATHEXT variable on windows (strip off the file extension on POSIX);
+ -  cd to RED_SPIDER_ROOT/work, mkdir if it doesn't exist;
+ -  drop rscall, since rsshell can already mimic its behaviour;
+ -  show a short info message on launch.
+'''
 
 import os
 import sys
 import rscall
 
+def main (argv = None):
+    rscall.set_red_spider_root()
+    if argv and len(argv) > 1:                  # behave like rscall
+        return os.system(" ".join(argv[1:]))
+    if os.name == 'nt':                         # Windows
+        return os.system(os.getenv('COMSPEC', 'cmd.exe'))
+    else:                                       # POSIX assumed
+        return os.system(os.getenv('SHELL', 'bash'))
+
 if __name__ == "__main__":
-	rscall.set_red_spider_root()
-	os.system(os.getenv('SHELL', 'bash'))
+	sys.exit(main(sys.argv))
