72fd08973bb07346527006a3b95b11eb7921be07
Neil Forrester <nforrest@mit.edu>
2012-04-12 11:04:14 -0400

    added json-parse.py
    Usage example:
    
    > ./rsshell.py
    > ./xkcd-search.py '(?i)spider' | ./xkcd-fetch.py | ./json-parse.py comic_title
    Red spiders
    Red Spiders 2
    Counter-Red Spiders
    Five Thirty
    Red Spiders Cometh
    Bad Timing
    xkcd Loves the Discovery Channel
    Control

---
+++ src/json-parse.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+# Copyright 2012 Neil Forrester
+# Licensed under the Red Spider Project License.
+# See the License.txt that shipped with your copy of this software for details.
+
+import json
+import sys
+
+if __name__ == "__main__":
+	for line in map(lambda json_dict: json_dict[sys.argv[1]], map(json.loads, sys.stdin.readlines())):
+		print line
