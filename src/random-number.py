bd20c4ee7d7affd97c0c21f383053b59c9d52641
JonETMeans <music2bgr8@yahoo.com>
2013-06-02 21:34:32 -0700

    Random number generator

---
+++ src/random-number.py
@@ -0,0 +1,7 @@
+import argparse
+
+print 4
+
+parser = argparse.ArgumentParser(description='Number chosen by fair dice roll. '
+                                 'Guaranteed to be random.')
+parser.parse_args()
