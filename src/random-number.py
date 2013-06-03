14f60cd292bd5853f259ba3ebaf599657ffe68a6
JonETMeans <music2bgr8@yahoo.com>
2013-06-02 21:39:27 -0700

    Attribution

--- src/random-number.py
+++ src/random-number.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python2
+
+# Copyright 2013 Jonathan E. T. Means
+# Licensed under the Red Spider Project License.
+# See the License.txt that shipped with your copy of this software for details.
+
+
 import argparse
 
 print 4
