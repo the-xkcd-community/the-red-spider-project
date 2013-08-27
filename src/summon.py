7187dbb7ca378726e2b58dc052dbc150582f9a24
Julian Gonggrijp <j.gonggrijp@gmail.com>
2013-08-27 22:32:49 +0200

    Use shell in subprocess.call for Windows.

--- src/summon.py
+++ src/summon.py
@@ -12,8 +12,10 @@ See the License.txt that shipped with your copy of this software for details.
 import sys
 import subprocess
 
+useshell = False
 if sys.platform.startswith('win32'):
     summoning_command = ['start']
+    useshell = True
 elif sys.platform.startswith('darwin'):
     summoning_command = ['open']
 else:  # linux assumed
@@ -24,7 +26,7 @@ def main (argv = None):
         print(usage_msg)
     else:
         for filename in argv:
-            subprocess.call(summoning_command + [filename])
+            subprocess.call(summoning_command + [filename], shell = useshell)
 
 usage_msg = '''
 Call me with a space-separated list of file paths and I'll summon
