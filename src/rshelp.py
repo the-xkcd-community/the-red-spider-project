3e08763474c3763c7d2634b3feb9d09c44dcca28
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2013-10-13 19:24:57 +0000

    Added "rshelp <command>" syntax.

--- src/rshelp.py
+++ src/rshelp.py
@@ -4,8 +4,12 @@
 # Licensed under the Red Spider Project License.
 # See the License.txt that shipped with your copy of this software for details.
 
-import os
+import os, sys
 
-print("The following commands are available in your version of The Red Spider Project:")
-for cmd in os.listdir(os.path.dirname(os.path.realpath(__file__))):
-    print(cmd)
+if len(sys.argv) == 1:
+    print("The following commands are available in your version of The Red Spider Project:")
+    for cmd in os.listdir(os.path.dirname(os.path.realpath(__file__))):
+        print(cmd)
+else:
+    manfile = open(os.path.dirname(os.path.realpath(__file__)) + '/../doc/' +  sys.argv[1] + '.txt', 'r')
+    print(manfile.read())
