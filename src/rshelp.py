4a28de44f579683b1e74e08de1c36c499fc59f15
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2013-10-16 03:12:35 +0000

    Fixed windows file extension issue (?)

--- src/rshelp.py
+++ src/rshelp.py
@@ -12,15 +12,18 @@ documentedcmds = []
 if len(sys.argv) == 1:
     print("The following commands are available in your version of The Red Spider Project:")
     for cmd in os.listdir(os.path.join(os.getenv("RED_SPIDER_ROOT"), "bin")):
-        print(cmd)
+        if not os.name == 'nt': # POSIX Assumed.
+            print(cmd)
+        else:
+            print(cmd.split(".")[0]) # Assuming you won't have commands like "__AUTOEXEC.BAT.MY%20OSX%20DOCUMENTS-INSTALL.EXE.RAR.INI.TAR.DOÇX.PHPHPHP.XHTML.TML.XTL.TXXT.0DAY.HACK.ERS_(1995)_BLURAY_CAM-XVID.EXE.TAR.[SCR].LISP.MSI.LNK.ZDA.GNN.WRBT.OBJ.O.H.SWF.DPKG.APP.ZIP.TAR.TAR.CO.GZ.A.OUT.EXE", this should work.
 else:
+    backuphelp = False
     try:
         docfile = open(os.path.join(os.getenv("RED_SPIDER_ROOT"), "config", "doc.txt"))
         for cmd in docfile.read().splitlines():
             documentedcmds.append(cmd.split(" ", 1))
     except:
-        print("Hey you!\n\nI couldn't find the list of commands that I use to generate help files.  It's a shot in the dark, but the best that I can give you is this:")
-        os.system(sys.argv[1] + " -h")  # This might be a bad idea if the user does "rshelp rshelp"...
+        backuphelp = True
     documented = False
     try:
         manfile = open(os.path.join(os.getenv("RED_SPIDER_ROOT"), "doc", sys.argv[1] + ".txt"), 'r')
@@ -31,4 +34,9 @@ else:
                 documented = True
                 os.system(cmd[1])
         if not documented:
-            print("Hey you!\n\nI couldn't find any documentation for that command.  I looked in the /doc directory and did '" + sys.argv[1] + " -h', but they didn't help.\nrshelp can no longer help you, but if you master the spectral wolf, he will guide you.  Godspeed.") # http://xkcd.com/461/
+            if not backuphelp:
+                print("Hey you!\n\nI couldn't find any documentation for that command.  I looked in the /doc directory and did '" + sys.argv[1] + " -h', but they didn't help.\nrshelp can no longer help you, but if you master the spectral wolf, he will guide you.  Godspeed.") # http://xkcd.com/461/
+            else:
+                print("Hey you!\n\nI couldn't find the list of commands that I use to generate help files.  It's a shot in the dark, but the best that I can give you is this:")
+                os.system(sys.argv[1] + " -h")
+                
