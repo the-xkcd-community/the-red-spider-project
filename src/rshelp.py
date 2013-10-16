5313e4504d0389ca73c2f7771c9bfc61677e12b7
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2013-10-16 01:51:38 +0000

    Made rshelp work with files in /doc.

--- src/rshelp.py
+++ src/rshelp.py
@@ -22,13 +22,13 @@ else:
         print("Hey you!\n\nI couldn't find the list of commands that I use to generate help files.  It's a shot in the dark, but the best that I can give you is this:")
         os.system(sys.argv[1] + " -h")  # This might be a bad idea if the user does "rshelp rshelp"...
     documented = False
-    for cmd in documentedcmds:
-        if cmd[0] == sys.argv[1]:
-            documented = True
-            os.system(cmd[1])
-    if not documented:
-        try:
-            manfile = open(os.path.join(os.getenv("RED_SPIDER_ROOT"), "config", "doc.txt"), 'r')
-            print(manfile.read())
-        except FileNotFoundError:
+    try:
+        manfile = open(os.path.join(os.getenv("RED_SPIDER_ROOT"), "doc", sys.argv[1] + ".txt"), 'r')
+        print(manfile.read())
+    except FileNotFoundError:
+        for cmd in documentedcmds:
+            if cmd[0] == sys.argv[1]:
+                documented = True
+                os.system(cmd[1])
+        if not documented:
             print("Hey you!\n\nI couldn't find any documentation for that command.  I looked in the /doc directory and did '" + sys.argv[1] + " -h', but they didn't help.\nrshelp can no longer help you, but if you master the spectral wolf, he will guide you.  Godspeed.") # http://xkcd.com/461/
