b1f12b8f465daac44c482351b0714ca23e43dd95
rytonbay <brytonmoeller@unknown881fa100f9dc.gateway.2wire.net>
2013-10-22 21:33:11 -0700

    Edits made
    i still have work to do on other files but the command should be
    looking better

--- src/Geico.py
+++ src/Geico.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python2
 
 # Copyright 2013 Bryton Moeller
 # Licensed under the Red Spider Project License.
@@ -7,38 +7,36 @@
 # Wesely will comment on my use of tabs instead of 4 spaces
 
 golfbag = {"Putter":[0.1,175],"5iron":[0.15,100],"7iron":[0.17,100],"Wood":[0.22,225],"Driver":[0.26,350]}
-# filling the golf bag			club                      %off                  price
-#									                    insurance
+# filling the golf bag          club                      %off                  price
+#                                                       insurance
 
-inttrue = False
-while(inttrue == False):
-	insurancecost = raw_input("How much does your insurance currently cost?:")
-	# pre-golfclub cost
-	if (isinstance(insurancecost, int)) == False:
-		print(insurancecost + ' is not an integer.')
-	else:
-		inttrue = True
-# integer checking
+# pre-golfclub cost
+try:
+    insurancecost = int(raw_input("How much does your insurance currently cost?:"))
+except IndentationError:
+    print("that's not an integer")
+# integer checking using an error i know i wont get
 
-
-clubchoice = raw_input("Which golf club would you like to use to threaten your agent?\nPutter costs 175$ but saves you 10 percent on your insurance.\n5iron costs 100$ but saves you 15 percent on your insurance.\n7iron costs 100$ but saves you 17 percent on your insurance.\nWood costs 225$ but saves you 22 percent on your insurance.\nDriver costs 350$ but saves you 26 percent on your insurance.")
+print("Putter costs 175$ but saves you 10 percent on insurance.\n")
+print("5iron costs 100$ but saves you 15 percent on insurance.\n")
+print("7iron costs 100$ but saves you 17 percent on insurance.\nWood costs 225$ but saves you 22 percent on insurance.\nDriver costs 350$ but saves you 26 percent on insurance.")
+clubchoice = raw_input("Which golf club would you like to use to threaten your agent?\n")
 # asks user for club choice and gives info
-if(clubchoice != "Putter" or "5iron" or "7iron" or "Wood" or "Driver"):
-	print "That's not a valid club choice."
-else:
+validchoice = False
+while(validchoice == False):
+    if(clubchoice not in ("Putter", "5iron", "7iron", "Wood", "Driver")):
+        print("That's not a valid club choice.")
+        clubchoice = raw_input("Which golf club would you like to use to threaten your agent?\n")
+    else:
+        validchoice = True
 # making sure the user made a valid club choice
 
-
-	intt2 = False
-	while(int2 == False):
-		strength = raw_input("how intimidating can you look with a " + golfbag[clubchoice] + "? (1-10): ")
-		# gets scaryness data
-		if (isinstance(strength, int)) == False:
-			print (strength + ' is not an integer')
-		else:
-			int2 = True
+try:
+    strength = int(raw_input("how intimidating can you look with a " + golfbag[clubchoice] + "? (1-10): "))
+    # gets scaryness data
+except IndentationError:
+    print("that's not an integer")
 # integer checking
+totalcost = insurancecost * (1 - (golfbag[clubchoice][0] + (strength/100)))
 
-	totalcost = insurancecost * (1 - (golfbag[clubchoice][0] + (strength/100)))
-
-print "Your insurance will cost " + totalcost + "$, but you will have to pay " + golfbag[clubchoice][1] + "$ to purchace your club.\n"
+print("Your insurance will cost ", totalcost, "$, but you will have to pay ", golfbag[clubchoice][1], "$ to purchace your club.\n")
