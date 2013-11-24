dff0916823efe511ab911fecc885b6bd4da1ad9e
sleepingdrone <hydrotoxin3@gmail.com>
2013-11-24 16:35:02 +0100

    Implemented webscraping and date adjustment

--- src/geohash.py
+++ src/geohash.py
@@ -5,14 +5,17 @@ from __future__ import print_function
 import os
 import hashlib
 import re
+from urllib import urlopen
 
 import time
+import datetime
 import argparse
 import json
 #~ import webbrowser
     
     
 DEFAULTS_FILE = os.path.join(os.getenv("RED_SPIDER_ROOT"), "work", "geohash", "defaults")
+URL_DOW = r"https://www.google.com/finance/historical?cid=983582&startdate={}&enddate={}"
 
 def geohash(latitude, longitude, datedow):
     '''Compute geohash() using the Munroe algorithm.
@@ -66,12 +69,30 @@ def make_datedow(date, dow):
     return "{}-{:.2f}".format(date, dow)
 
 def get_date_of_dow(date, coords):
-    #TODO implement w30 rule, etc.
+    date = datetime.date(*date[:3])
+    if 0 > coords[1] > -30:
+        date -= datetime.timedelta(days=1)
+    wkday = date.weekday()
+    if wkday > 4:
+        date -= datetime.timedelta(wkday - 4)
+    date = date.timetuple()
     return date
 
 def get_dow(date):
-    #TODO implement dow fetching from finance.google.com
-    return '10458.68'
+    try:
+        regex = '<td class="lm">.*{}.*{}\n<.+>(.+)'.format(date[2], date[0])
+        regex = re.compile(regex)
+        time_format = "%Y%m%d"
+        enddate = datetime.date(*date[:3])
+        startdate = enddate - datetime.timedelta(weeks=1)
+        request = URL_DOW.format(*[x.strftime(time_format) for x in (startdate, enddate)])
+        req = urlopen(request)
+        page = req.read()
+        req.close()
+        dow = float(regex.search(page).groups()[0].replace(",", ""))
+        return dow
+    except:
+        return
 
 def get_location_coords(gen_location):
     #TODO implement location lookup
@@ -112,12 +133,20 @@ if __name__ == "__main__":
     
     date = parse_date(args.date) if args.date else time.localtime()
     date_of_dow = get_date_of_dow(date, args.location)
+    datedow = None
     
     if not args.dow:
+        print("Fetching DOW from the web..")
         args.dow = get_dow(date_of_dow)
-    datedow = make_datedow(date_of_dow, args.dow)
+    
+    if args.dow:
+        datedow = make_datedow(date_of_dow, args.dow)
+    else:
+        print("drats!")
     
     if args.location and datedow:
+        print()
+        print("Input: {}".format(datedow))
         unpack = args.location + [datedow]
         geo_location = geohash(*unpack)
-        print("{}, {}".format(*geo_location))
+        print("Output: {}, {}".format(*geo_location))
