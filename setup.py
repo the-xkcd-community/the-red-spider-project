a5f2d3f6426cc04451036ed5a84b996a1069f964
sleepingdrone <hydrotoxin3@gmail.com>
2014-07-31 00:16:16 +0200

    Add geohash to setup.py

--- setup.py
+++ setup.py
@@ -37,11 +37,12 @@ documented_cmds = [["xkcd-fetch",    "xkcd-fetch --help"],
                    ["summon",        "summon"],
                    ["fortune",       "fortune --help"],
                    ["godel",         "godel"],
-                   ["random-number", "random-number --help"]]
+                   ["random-number", "random-number --help"],
+                   ["geohash", "geohash --help"]]
 
 executable_scripts = [  'json-parse.py', 'xkcd-fetch.py', 'xkcd-search.py',
                         'level_up.py', 'summon.py', 'fortune.py', 'godel.py',
-                        'random-number.py', 'rshelp.py', 'geico.py' ]
+                        'random-number.py', 'rshelp.py', 'geico.py', 'geohash.py' ]
 
 python_modules = 'src/xkcd-fetch.py src/level_up.py'.split()
 
