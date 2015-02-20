b544d69cf6ba0ca45c886187d75bd50f70803741
Michael Gardner <darkhelmet17@gmail.com>
2015-02-19 19:52:07 -0800

    Adding base file for counterstrike program

---
+++ src/counterstrike.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+print "Welcome to text-only Counterstrike"
+print "You are in a dark, outdoor map."
+
+while True:
+	input = raw_input("> ")
+	
+	if input == "GO NORTH" or input == "GO SOUTH" or input == "GO EAST" or input == "GO WEST"
+		print "You have been pwned by a grue."
+		break
+	else
+		print "Unknown command"
